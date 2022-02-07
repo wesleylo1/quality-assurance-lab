64be6c1f3c78f1721e4968a5049fe32f3a68c85d
Wesley Lo <wesleylo1995@gmail.com>
2022-02-07 15:26:36 -0600

    added 3 other math functions

--- functions.js
+++ functions.js
@@ -9,5 +9,17 @@ module.exports = {
 
     add: (num1,num2) => {
         return num1 + num2
+    },
+
+    multiply: (num1,num2) => {
+        return num1 * num2
+    },
+
+    divide: (num1,num2) => {
+        return num1 / num2
+    },
+
+    subtract: (num1,num2) => {
+        return num1 - num2
     }
 }
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const { returnTwo, greeting, add } = require('./functions.js')
+const { returnTwo, greeting, add, multiply, divide, subtract } = require('./functions.js')
 
 test('equal two',() => {
     expect(returnTwo()).toEqual(2)
@@ -14,4 +14,23 @@ test('add two number', () => {
     expect(add(1,2)).toEqual(3)
 
     expect(add(5,9)).toEqual(14)
+})
+
+
+describe('Math functions', () => {
+    test('add', () => {
+        expect(add(10,5)).toEqual(15)
+    })
+
+    test('multiply', () => {
+        expect(multiply(10,5)).toEqual(50)
+    })
+
+    test('divide', () => {
+        expect(divide(10,5)).toEqual(2)
+    })
+
+    test('subtract', () => {
+        expect(subtract(10,5)).toEqual(5)
+    })
 })
